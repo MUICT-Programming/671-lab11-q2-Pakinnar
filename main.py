f16a8e9e2cd756f60cb8636e9f5a763bd36447c2
Pakinnar <pakin.nar@student.mahidol.edu>
2024-10-20 13:35:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # YOUR CODE HERE
-# YOUR CODE HERE
 list1=[]
 list2=[]
 n=int(input())
@@ -12,31 +11,33 @@ for i in range(n):
     a=int(input())
     list2.append(a)
 
-def summation(a, b):
+def summation(list1, list2):
     list_sum=[]
     global n
+    #คือการเรียกใช้ keyword เพราะไม่สามารถส้รางใน function ได้ 
     for i in range(n):
-        sum=a[i]+b[i]
+        sum=list1[i]+list2[i]
         list_sum.append(sum)
     return list_sum
-
+    #ค่าในการบวกกันของlist 
 list_sum = summation(list1,list2)
-
-def find_min_max(a):
-    max=a[0]
-    min=a[0]
-    for i in a:
+#ต้องเอาไปคิดต่อใน function ต่อไป
+def find_min_max(list):
+    max=list[0]
+    min=list[0]
+    for i in list:
         if i>max:
             max=i
-        else:
-            continue
-    for i in a:
+        #else:
+            #continue
+    for i in list:
         if i<min:
             min=i
-        else:
-            continue
+        #else:
+            #continue
     min_max=(min,max)
     return min_max
+    #การหาค่าจาก min และ max 
 
 print(summation(list1,list2))
 print(find_min_max(list_sum))
